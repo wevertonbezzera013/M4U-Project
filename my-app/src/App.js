125b5d43d0878d5778fe2887ed4702c66cea5549
Weverton <wevertonbezzera013@gmail.com>
2021-03-07 18:52:34 -0300

    preparing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/navbar/navbar'
+import Footer from './components/footer/footer'
+import Main from './components/main/main'
+import Header from './components/header/header'
 
 function App() {
-  return ();
+  return (
+    <div>
+      <NavBar/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './footer.css'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <h1>FOOTER</h1>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
