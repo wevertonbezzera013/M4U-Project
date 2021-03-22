aaeb71e772da930ca19dc8ef67414f38d2a99feb
julietefreitas <juliete.gomes17@gmail.com>
2021-03-22 17:17:37 -0300

    + porta pro heroku

--- source/app.js
+++ source/app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const usuarioController = require('./controllers/usuario-controller');
-const enderecoController = require('./controllers/endereco-controller');
-const chipController = require('./controllers/chip-controller');
-const cartaoController = require('./controllers/cartao-controller');
-const bd = require('./infra/conexaoMysql')
+const express = require("express");
+const bodyParser = require("body-parser");
+const usuarioController = require("./controllers/usuario-controller");
+const enderecoController = require("./controllers/endereco-controller");
+const chipController = require("./controllers/chip-controller");
+const cartaoController = require("./controllers/cartao-controller");
+const bd = require("./infra/conexaoMysql");
 
-const port = 5000
-const app = express()
-app.use(bodyParser.json())
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(bodyParser.json());
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-usuarioController(app, bd)
-enderecoController(app, bd)
-chipController(app, bd)
-cartaoController(app, bd)
+usuarioController(app, bd);
+enderecoController(app, bd);
+chipController(app, bd);
+cartaoController(app, bd);
 
-app.listen(port, () => console.log(`Listen on port ${port}`))
+app.listen(port, () => console.log(`Listen on port ${port}`));
