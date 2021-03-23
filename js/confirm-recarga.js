7f7bc68029dfa8d8cc25246344a9b9d057165b77
julietefreitas <juliete.gomes17@gmail.com>
2021-03-23 17:57:32 -0300

    mascara do campo de cartao

--- js/confirm-recarga.js
+++ js/confirm-recarga.js
@@ -14,19 +14,42 @@ const validade = document.getElementById("validade_cartao");
 
 
 const mascaraValidacao = (value, pattern) => {
-	console.log("oi");
 	let i = 0;
 	const v = value.toString();
 	return pattern.replace(/#/g, () => v[i++] || "");
 };
-const mascaraNumero = (value, pattern) => {
+ const mascaraNumero = (value, pattern) => {
 	let i = 0;
 	const v = value.toString();
 	return pattern.replace(/#/g, () => v[i++] || "");
-};
+}; 
 
+
+const mascaraNumeroCartao =(number)=>{
+	const string = number.toString();
+	const arrayNovo = [];
+	let cartaoFormatado ="";
+
+for (let i=0 ; i< string.length; i++) {
+  arrayNovo[i] = parseInt(string[i]);
+}
+console.log(arrayNovo)
+
+for (let i = 0; i <arrayNovo.length; i++){
+  if(i > 3 && i <12){
+    console.log("aqui")
+    cartaoFormatado += "X";
+  }
+  else {
+    cartaoFormatado += arrayNovo[i];
+  }
+} 
+return cartaoFormatado;
+
+
+}
 
 recarga.innerHTML = valor;
 telefone.innerHTML = mascaraNumero(msisdn.slice(2, 13), "(##)#####-####");
-cartao.innerHTML = number;
+cartao.innerHTML = mascaraNumeroCartao(number);
 validade.innerHTML = mascaraValidacao(expirationDate, "##/##");
