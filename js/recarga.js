65ec19630cf1aa0d0e34bdea0a80bf4a14375d4a
julietefreitas <juliete.gomes17@gmail.com>
2021-03-23 17:56:28 -0300

    + alinhamento de campos pra proxima pagina

--- js/recarga.js
+++ js/recarga.js
@@ -17,6 +17,7 @@ botaoClick.addEventListener("click", (e) => {
 	const select = document.getElementById("bandeira_cartao");
 	const value = select.options[select.selectedIndex].value;
 	console.log(valorInt)
+
 	const cartaoUser = {
 		"customer": { msisdn: parseInt("55" + numeroEasyC.value) },
 		"payment": {
@@ -29,15 +30,56 @@ botaoClick.addEventListener("click", (e) => {
 			"brand": value,
 		},
 	};
-	console.log(cartaoUser);
-	response(cartaoUser);
-	window.localStorage.setItem("cartao", JSON.stringify(cartaoUser));
-	window.location.href = "../html/confirm-recarga.html"; 
-	console.log(window.localStorage.getItem("valor"));
+	const result = requisicaoCartao(cartaoUser)
+	.then((response) =>{
+	 const {message, code} = response;
+	 verificaResponse(message, code, cartaoUser);
+
+	})
+	.catch((err)=>{
+		console.log(err.message);
+	})		
+	//window.location.href = "../html/confirm-recarga.html"; 
+	
 });
 
-const response = async (body) =>
-	await fetch("http://4r4z1.mocklab.io/v1/payment", {
+const verificaResponse = (mensagem, codigo,cartaoUser) =>{
+	if(codigo == "EXPIREDDATE"){
+		exibeModalNegCompra(mensagem);	
+	}else if (codigo == "DENIED") {
+		exibeModalNegCompra(mensagem);
+	}else if (codigo == "INTERNAL") {
+		exibeErroInterno(mensagem);
+	}else {
+		setTimeout(()=>{
+			window.localStorage.setItem("cartao", JSON.stringify(cartaoUser));
+	    window.location.href = "../html/confirm-recarga.html";
+		},3000)
+		
+	}
+}	
+
+const exibeModalConfirmacao = (msg) =>{
+	  let elems = document.querySelector('#modal-confirm');
+		let instances = M.Modal.init(elems);
+		instances.open();
+} 
+
+const exibeModalNegCompra =(msg) =>{
+    let elems = document.querySelector('#modal-compraNegada');
+		let instances = M.Modal.init(elems);
+		document.querySelector("#conteudo-modal-second-neg").innerHTML = `Poxa que triste, não podemos concluir a compra pois seu <b> ${msg.toUpperCase()} </b> Por favor entre em contato com o seu banco.`;
+		instances.open();
+}
+const exibeErroInterno= (msg)=>{
+		let elems = document.querySelector('#modal-compraNegada');
+		let instances = M.Modal.init(elems);
+		document.querySelector("#conteudo-modal-second-neg").innerHTML = `Poxa que triste, não podemos concluir a compra pois ocorreu um <b> ${msg.toUpperCase()} </b> Por favor entre em contato com o seu banco.`;
+		instances.open();
+}
+
+const requisicaoCartao = async (body) => {
+	const response = await fetch("http://4r4z1.mocklab.io/v1/payment", {
 		method: "POST",
 		headers: {
 			Accept: "application/json",
@@ -47,13 +89,21 @@ const response = async (body) =>
 		body: JSON.stringify(body),
 		mode: "cors",
 		cache: "default",
-	})
-		.then((data) => {
+	});
+	if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+	const responseJson = await response.json();
+	return responseJson;
+}
+	
+		/* .then((data) => {
 			return data.json();
 		})
 		.then((response) => {
-			console.log(response);
+			return response;
 		})
 		.catch((err) => {
-			console.log(err);
-		});
+			console.log(err); */
+	//	});
